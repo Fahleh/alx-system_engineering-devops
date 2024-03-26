2709404db807b081493d1dbc57f9de086c814fac
Eddy <eddyukpong@gmail.com>
2024-03-26 15:20:38 -0700

    Fixes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,7 +12,7 @@ file {'/var/www/html/index.nginx-debian.html':
 file_line {'configure redirection':
     path  =>  '/etc/nginx/sites-available/default',
     after =>  'server_name _;',
-    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}\n",
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=2bI8XsdNb-c&list=PLVipAi3U-Jos-gFKT434UTj2D9x7t83IH;\n\t}\n",
 }
 
 service {'nginx':
